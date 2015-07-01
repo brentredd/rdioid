3f5b06403823736ec8acd94faa62391374fa469d
Brent Redd <reddshack@gmail.com>
2015-07-01 01:20:19 -0600

    update with requires, constants, and config

--- lib/rdioid.rb
+++ lib/rdioid.rb
@@ -1,5 +1,23 @@
+require "httpclient"
+require "json"
+
+require "rdioid/client"
+require "rdioid/config"
 require "rdioid/version"
 
 module Rdioid
-  # Your code goes here...
+  AUTHORIZATION_URL = 'https://www.rdio.com/oauth2/authorize/'
+  BASE_URL = 'https://services.rdio.com/'
+  API_ENDPOINT = 'api/1/'
+  OAUTH_TOKEN_ENDPOINT = 'oauth2/token/'
+  OAUTH_DEVICE_CODE_ENDPOINT = 'oauth2/device/code/generate/'
+
+  class << self
+    attr_accessor :config
+  end
+
+  def self.configure
+    self.config ||= Rdioid::Config.new
+    yield(config)
+  end
 end
