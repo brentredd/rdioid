a8cb957469b53b5bef3c377df65fb3ddc9fda05f
Brent Redd <reddshack@gmail.com>
2015-09-29 00:41:51 -0600

    add spec helper method to set config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rdioid'
+
+def set_config_values
+  Rdioid.configure do |config|
+    config.client_id = 'test_id'
+    config.client_secret = 'test_secrest'
+    config.redirect_uri = 'http://test_redirect_uri/'
+  end
+end
